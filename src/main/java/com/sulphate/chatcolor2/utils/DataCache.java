a6a3896fe4f77501f43f1e032cc2de6aff18ee6a
MCSulphate <matthewl64@hotmail.co.uk>
2017-05-12 12:54:29 +0100

    Continued work on caching data

--- src/main/java/com/sulphate/chatcolor2/utils/DataCache.java
+++ src/main/java/com/sulphate/chatcolor2/utils/DataCache.java
@@ -10,45 +10,86 @@ public class DataCache {
 
     private HashMap<String, String> settings = new HashMap<>();
     private HashMap<String, String> colours = new HashMap<>();
-    private FileConfiguration config = MainClass.get().getConfig();
+    private HashMap<String, String> messages = new HashMap<>();
+    private HashMap<String, String> defcodes = new HashMap<>(); //TODO: Set default defcode of 0 on join.
+    private FileConfiguration playerlist;
 
     public DataCache() {
         loadAllData();
     }
 
-    public void loadAllData() {
-        loadMessages();
-        loadSettings();
-        loadColours();
+    public boolean loadAllData() {
+        loadPlayerList();
+        return (loadMessages() && loadSettings() && loadPlayerData());
     }
-    public void loadSettings() {
+    public void loadPlayerList() {
+        playerlist = FileUtils.getPlayerListConfig();  
+    }
+    public boolean loadSettings() {
         settings = new HashMap<>();
+        
+        reloadConfig();
         ConfigurationSection settingsSection = MainClass.get().getConfig().getConfigurationSection("settings");
         Map<String, Object> settings = settingsSection.getValues(false);
+        
         for(Map.Entry<String, Object> entry : settings.entrySet()) {
             try {
-                settings.put(entry.getKey(), entry.getValue());
+                this.settings.put(entry.getKey(), entry.getValue());
             } catch(Exception e) {
                 MainClass.setPluginEnabled(false);
+                Bukkit.getLogger().warning(CCStrings.errdet + "§bError loading settings. Please check they are all correct!");
+                return false;
             }
         }
+        
+        return true;
     }
-    public void loadColours() {
-
+    public boolean loadMessages() {
+        messages = new HashMap<>();
+        
+        reloadConfig();
+        ConfigurationSection messagesSection = MainClass.get().getConfig().getConfigurationSection("messages");
+        Map<String, Object> messages = messagesSection.getValues(false);
+        
+        for(Map.Entry<String, Object> entry : messages.entrySet()) {
+            try {
+                this.messages.put(entry.getKey(), entry.getValue());
+            } catch(Exception e) {
+                MainClass.setPluginEnabled(false);
+                Bukkit.getLogger().warning(CCStrings.errdet + "§bError loading messages. Please check they are all correct!");
+                return false;
+            }
+        }
+        
+        return true;
     }
-    public void loadMessages() {
-
+    public boolean loadPlayerData() {
+        for (string key : playerlist.getKeys()) {
+               
+        }
     }
 
     public void saveAllData() {
         saveSettings();
-        saveColours();
+        savePlayerData();
+    }
+    public void savePlayerList() {
+        FileUtils.saveConfig(playerlist, FileUtils.getPlayerList());   
     }
     public void saveSettings() {
+        reloadConfig();
+        FileConfiguration config = Main.get().getConfig()
+        for (Map.Entry<String, String> entry : settings.entrySet()) {
+            config.set(entry.getKey(), entry.getValue());
+        }
+        Main.get().saveConfig();
+    }
+    public void savePlayerData() {
 
     }
-    public void saveColours() {
-
+    
+    private void reloadConfig() {
+        MainClass.get().reloadConfig();   
     }
 
 }
